fed23ae6c589b6bd19b4cd1791ee8fa8a9197d58
Xavier Sala <utrescu@gmail.com>
2016-07-31 21:51:32 +0200

    Primers intents de fer tests

--- app/service/colorsservice.js
+++ app/service/colorsservice.js
@@ -5,7 +5,7 @@ function ColorsService() {
   this.getAll = function (callback) {
     connection.getAll(function (err, result) {
       if (err) {
-        result = "{status: 1, message: \"" + result + "\"}"
+        result = JSON.parse('{"status": 1, "message": \"' + result + '"}');
       }
       callback(result);
     });
@@ -15,10 +15,10 @@ function ColorsService() {
   this.get = function (nom, callback) {
     connection.get(nom, function (err, result) {
       if (err) {
-        var resultat = "{status: 1, message: \"" + result + "\"}"
+        var resultat = JSON.parse('{"status": 1, "message": \"' + result + '"}');
       } else {
         if (result.length == 0) {
-          var resultat = "{status: 1, message: 'No trobat'}";
+          var resultat = JSON.parse('{"status": 1, "message": "No trobat"}');
         } else {
           var resultat = result[0];
         }
---
+++ test/colorservicetest.js
@@ -0,0 +1,55 @@
+
+var chai = require("chai");
+var should = require('chai').should();
+var expect = chai.expect;
+var assert = chai.assert;
+var servei = require("../app/service/colorsservice");
+var connection = require("../app/service/connection");
+
+/**
+ * Comprova que el servei està funcionant.
+ * 
+ * Falta descobrir com es fan els 'mocks'
+ *  - Per fer mock de connection
+ */
+describe("Color service test", function () {
+
+    before(function(done) {
+        connection.init();
+        done();
+    }) 
+
+    describe("Recupera tots els colors", function () {
+        it("comprova que es reben els colors", function (done) {
+            var espero = JSON.parse('{"id":71,"nom":"Vermell","rgb":"#FF0000"}');
+            servei.getAll(function(data) {                
+                assert(Array.isArray(data), 'La llista de colors ha de ser un array');
+                // expect(data).to.contain(espero);
+                // assert.containSubset(data, espero);
+                done();
+            })
+        });
+    });
+
+    describe("Recupera un color", function () {
+        it("comprova que es recupera el color vermell", function (done) {
+            var espero = JSON.parse('{"id":71,"nom":"Vermell","rgb":"#FF0000"}');
+            servei.get("vermell", function(data) {
+                should.exist(data); 
+                data.should.have.property('nom', 'Vermell');
+                assert.notStrictEqual(data, espero);
+                done();
+            });
+        });
+
+        it("comprova que un color que no existeix dóna error", function(done) {
+            const color_no_trobat = "No trobat";
+            servei.get("patata", function(data) {
+                data.should.not.have.property('nom');
+                data.should.have.property('message', color_no_trobat);
+                done();
+            });
+        });
+        
+    });
+});
