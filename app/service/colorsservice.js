310898144bd1049b3bb201c514a84e6717ae718c
Xavier Sala <utrescu@gmail.com>
2020-06-21 17:40:16 +0200

    Bugfix package vulnerabilities & upgrade

--- app/service/colorsservice.js
+++ app/service/colorsservice.js
@@ -1,11 +1,11 @@
-var connection = require('./connection')
+var connection = require("./connection");
 
 function ColorsService() {
   // Obtenir tots els colors de la base de dades
   this.getAll = function (callback) {
     connection.getAll(function (err, result) {
       if (err) {
-        result = JSON.parse('{"status": 1, "message": \"' + result + '"}');
+        result = JSON.parse('{"status": 1, "message": "' + result + '"}');
       }
       callback(err, result);
     });
@@ -14,22 +14,21 @@ function ColorsService() {
   // Obtenir només el color especificat
   this.get = function (nom, callback) {
     connection.get(nom, function (err, result) {
-      if (err) {  
-        var error = true;     
-        var resultat = JSON.parse('{"status": 1, "message": \"' + result + '"}');
+      if (err) {
+        var error = true;
+        var resultat = JSON.parse('{"status": 1, "message": "' + result + '"}');
       } else {
         if (result.length == 0) {
-          var error = true;          
-          var resultat = JSON.parse('{"status": 1, "message": "No trobat"}');
-        } else {          
-          var error = false;
-          var resultat = result[0];
+          error = true;
+          resultat = JSON.parse('{"status": 1, "message": "No trobat"}');
+        } else {
+          error = false;
+          resultat = result[0];
         }
       }
       callback(error, resultat);
-    })
+    });
   };
-
 }
 
 module.exports = new ColorsService();
--- app/service/connection.js
+++ app/service/connection.js
@@ -1,51 +1,41 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
-var config = require('config');
-var dbconfig = config.get('Colors.database');
+var config = require("config");
+var dbconfig = config.get("Colors.database");
 
 function Connection() {
   this.pool = null;
 
   this.init = function () {
-    this.pool = mysql.createPool(
-      dbconfig
-    );
-  };
-
-  this.acquire = function (callback) {
-    this.pool.getConnection(function (err, connection) {
-      callback(err, connection);
-    });
+    this.pool = mysql.createPool(dbconfig);
   };
 
   this.getAll = function (callback) {
-    this.acquire(function (err, con) {
-      con.query('select * from color', function (err, result) {
-        con.release();
-        if (err) {
-          var resultat = err;
-        } else {
-          var resultat = result;
-        }
-        callback(err, resultat);
-      });
+    this.pool.query("select * from color", function (err, result) {
+      // con.release();
+      if (err) {
+        var resultat = err;
+      } else {
+        resultat = result;
+      }
+      callback(err, resultat);
     });
-  }
+  };
 
   this.get = function (nom, callback) {
-    this.acquire(function (err, con) {
-      con.query('select * from color where nom = ?', [nom], function (err, result) {
-        con.release();
-        if (err) {
-          var resultat = err;
-        } else {
-          var resultat = result;
-        }
-        callback(err, resultat);
-      })
-    })
-  }
-
+    this.pool.query("select * from color where nom = ?", [nom], function (
+      err,
+      result
+    ) {
+      // con.release();
+      if (err) {
+        var resultat = err;
+      } else {
+        resultat = result;
+      }
+      callback(err, resultat);
+    });
+  };
 }
 
 module.exports = new Connection();
